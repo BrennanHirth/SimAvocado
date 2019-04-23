7c4f4b8333365ebdcd2f1d5456db707f3f5bdb35
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-04-23 07:56:18 -0400

    Updates

--- SimAvocado/src/avocado/Avocado.java
+++ SimAvocado/src/avocado/Avocado.java
@@ -17,32 +17,20 @@ public class Avocado {
 	public String getColor() {
 		return color;
 	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-	public enumCondition getCondition() {
-		return condition;
-	}
-	public void setCondition(enumCondition condition) {
-		this.condition = condition;
-	}
+	public void setColor(String color) {this.color = color;	}
+	public enumCondition getCondition() {return condition;}
+	public void setCondition(enumCondition condition) {this.condition = condition;}
 	public Avocado(String color, enumCondition condition, int weightInGrams) {
 		setColor(color);
 		setCondition(condition);
 		setWeightInGrams(weightInGrams);
 	}
-	public int getWeightInGrams() {
-		return weightInGrams;
-	}
-	public void setWeightInGrams(int weightInGrams) {
-		this.weightInGrams = weightInGrams;
-	}
+	public int getWeightInGrams() {return weightInGrams;}
+	public void setWeightInGrams(int weightInGrams) {this.weightInGrams = weightInGrams;}
 	/***
 	 * Make this avocado spoiled. No one would possibly eat it.
 	 */
-	public void makeUseless() {
-		condition = enumCondition.useless;
-	}
+	public void makeUseless() {condition = enumCondition.useless;}
 	/***
 	 * Check to see if the avocado is useless
 	 * @return True if it's useless, false otherwise
