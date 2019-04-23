210e408b9d9ee63298091b6e57c6e99ec2c5f715
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-04-23 07:37:34 -0400

    Adding properties to Avocado

--- SimAvocado/src/avocado/Avocado.java
+++ SimAvocado/src/avocado/Avocado.java
@@ -10,5 +10,32 @@ package avocado;
  *
  */
 public class Avocado {
-
+	private String color;
+	enum enumCondition {fresh, ok, squishy, useless};
+	private enumCondition condition;
+	private int weightInGrams;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public enumCondition getCondition() {
+		return condition;
+	}
+	public void setCondition(enumCondition condition) {
+		this.condition = condition;
+	}
+	public Avocado(String color, enumCondition condition, int weightInGrams) {
+		setColor(color);
+		setCondition(condition);
+		setWeightInGrams(weightInGrams);
+	}
+	public int getWeightInGrams() {
+		return weightInGrams;
+	}
+	public void setWeightInGrams(int weightInGrams) {
+		this.weightInGrams = weightInGrams;
+	}
+	
 }
